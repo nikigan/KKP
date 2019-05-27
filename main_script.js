9302632c2fd5506c3e791f3bfb0de6355de9ccd8
Nikita Gankin <gankin.n@yandex.ru>
2019-05-27 21:20:14 +0700

    Ver2

--- main_script.js
+++ main_script.js
@@ -1,22 +1,63 @@
 let userToken = window.localStorage.getItem('userToken');
-let studObj;
-$.ajax({
-    type: "POST",
-    url: url+"student/get",
-    data: JSON.stringify({
-        text: '',
-        userToken: userToken
-    }),
-        contentType: "application/json"
-})
-    .done(function (data) {
-        studObj = JSON.parse(data).student;
-        $('#name').append($('<span class="justify-content-center">').append(`${studObj.surname} ${studObj.name} ${studObj.patronymic}`));
-        $('#group').text(`${studObj.groupname}`);
-        $('#birthday').text(`${studObj.birthday}`);
-        $('#email').text(`${studObj.email}`);
-        $('#recordBook').text(`${studObj.recordbook}`);
-    });
+let studObj = {
+    birthday: undefined,
+
+    };
+
+getData();
+groupMates();
+
+function getData() {
+    $.ajax({
+        type: "POST",
+        url: url + "student/get",
+        data: JSON.stringify({
+            text: '',
+            userToken: userToken
+        }),
+        contentType: "application/json",
+        converters: {
+            "* json": function (text) {
+                return $.parseJSON(text);
+            }
+        },
+        dataType: 'json'
+    })
+        .done(function (data) {
+            console.log(data);
+            studObj = data.student;
+            fillFields(studObj);
+        });
+}
+
+function fillFields(studObj) {
+    $('#name').append($('<span class="justify-content-center">').append(`${studObj.surname} ${studObj.name} ${studObj.patronymic}`));
+    $('#group').text(`${studObj.groupname}`);
+    $('#birthday').text(`${studObj.birthday}`);
+    $('#email').text(`${studObj.email}`);
+    $('#recordBook').text(`${studObj.recordbook}`);
+}
+
+function groupMates() {
+    $.ajax({
+        type: "POST",
+        url: url + "student/classmates",
+        data: JSON.stringify({
+            text: '',
+            userToken: userToken
+        }),
+        contentType: "application/json",
+        converters: {
+            "* json": function (text) {
+                return $.parseJSON(text);
+            }
+        },
+        dataType: 'json'
+    })
+        .done(function (data) {
+            console.log(data);
+        });
+}
 
 
 
